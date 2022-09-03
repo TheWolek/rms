a045d512124f5b8d8d2d2c9f81434a904ec09f9b
TheWolek <pawelm916@wp.pl>
2022-09-03 13:11:22 +0200

    insert order

--- orders.js
+++ orders.js
@@ -62,4 +62,97 @@ router.get("/active", (req, res) => {
   });
 });
 
+router.post("/", (req, res) => {
+  // recive {items: [INT, INT], paymentMethod: INT, takeAway: BOOL}
+  // return 400 if any parameter is missing
+  // return 500 if there was DB error
+  // return 200 with {}
+
+  if (
+    req.body.paymentMethod === undefined ||
+    req.body.paymentMethod === 0 ||
+    typeof req.body.paymentMethod !== "number"
+  ) {
+    return res
+      .status(400)
+      .json({ message: "pole paymentMethod jest wymagane" });
+  }
+
+  if (
+    req.body.takeAway === undefined ||
+    typeof req.body.takeAway !== "boolean"
+  ) {
+    return res.status(400).json({ message: "pole takeAway jest wymagane" });
+  }
+
+  if (req.body.items === undefined || req.body.items.length === 0) {
+    return res.status(400).json({ message: "pole items jest wymagane" });
+  }
+
+  function getLastOrderId() {
+    return new Promise(function (resolve, reject) {
+      let sql =
+        "SELECT displayOrderId FROM orders WHERE isClosed = 0 ORDER BY displayOrderId desc LIMIT 1";
+
+      connection.query(sql, function (err, rows) {
+        if (err) return reject(err);
+
+        resolve(rows);
+      });
+    });
+  }
+
+  function getInsertedOrderId(newID) {
+    return new Promise(function (resolve, reject) {
+      let takeAway = req.body.takeAway === true ? 1 : 0;
+      let sqlOrder = `INSERT INTO orders (displayOrderId, paymentMethod, takeAway, status, isClosed) VALUES (${newID}, ${req.body.paymentMethod}, ${takeAway}, "new", 0)`;
+
+      connection.query(sqlOrder, function (err, result) {
+        if (err) return reject(err);
+
+        resolve(result);
+      });
+    });
+  }
+
+  getLastOrderId()
+    .then(function (rows) {
+      const lastId = rows[0].displayOrderId;
+      const newID = lastId + 1;
+
+      getInsertedOrderId(newID)
+        .then(function (result) {
+          const orderInsertedId = result.insertId;
+          let items = "";
+
+          for (let i = 0; i < req.body.items.length; i++) {
+            const item = req.body.items[i];
+
+            items += `(${item}, ${orderInsertedId}, "new")`;
+            if (i < req.body.items.length - 1) items += ", ";
+          }
+
+          let sqlOrderDishes = `INSERT INTO dishesInOrders (dishId, orderId, dishStatus) VALUES ${items}`;
+
+          connection.query(sqlOrderDishes, function (err, result) {
+            if (err) return res.status(500).json(err);
+            res.status(200).json({
+              orderId: orderInsertedId,
+              displayOrderId: newID,
+              paymentMethod: req.body.paymentMethod,
+              takeAway: req.body.takeAway,
+              status: "new",
+              items: req.body.items,
+            });
+          });
+        })
+        .catch((err) => {
+          return res.status(500).json(err);
+        });
+    })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
+});
+
 module.exports = router;
